5218d8d2ad3975468035fecb77359dd0e6cb7292
T1m1 <Ruprecht.t@gmx.de>
2015-03-31 12:51:09 +0200

    add function call of userservice function.

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,10 @@
 /// <reference path="../typings/hapi/hapi.d.ts" />
 var Hapi = require('hapi');
 
+// TEST
+import User = require('./user/userservice');
+var db = new User.UserService("http://localhost:5984", "test");
+
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
 
