4d29fed5256df7028d5ed3c31f051352139d50f6
T1m1 <Ruprecht.t@gmx.de>
2015-04-02 11:46:04 +0200

    implement route myGroup

--- lib/plugins/group/groupPlugin.ts
+++ lib/plugins/group/groupPlugin.ts
@@ -1,5 +1,6 @@
 export class GroupPlugin {
     register:any;
+    databaseInstance:any;
 
     constructor() {
         this.register.attributes = {
@@ -9,7 +10,26 @@ export class GroupPlugin {
     }
 
     register(server, options, next) {
+        // TODO: refactor -> duplicate code as in user plugin
+        if (!options.databaseInstance) {
+            throw new Error('options.databaseInstance needs to be defined');
+        }
+        this.databaseInstance = options.databaseInstance;
+        var db = this.databaseInstance.database('app');
 
+
+        // get information about group from current user
+        server.route({
+            method: 'GET',
+            path: '/myGroups',
+            handler: (request, reply) => {
+                var userId = request.session.get('loggedInUser');
+                // TODO: doc param -> interface User
+                db.get(userId, function (err, doc) {
+                    reply(doc.subscribed_groups);
+                });
+            }
+        });
         next();
     }
 
